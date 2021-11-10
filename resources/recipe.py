01af4f4621d5475a1b8dadd8b4fc4b6a03e7dad2
Kelsey Maynard <kelsey@funnel.io>
2021-11-10 08:20:58 -0500

    Finishing up exercise 9

--- resources/recipe.py
+++ resources/recipe.py
@@ -56,3 +56,15 @@ class RecipeResource(Resource):
         recipe.directions = data['directions']
 
         return recipe.data, HTTPStatus.OK
+
+class RecipePublishResource(Resource):
+
+    def put(self, recipe_id):
+        recipe = next((recipe for recipe in recipe_list if recipe.id == recipe_id), None)
+
+        if recipe is None:
+            return{'message': 'Recipe not found'}, HTTPStatus.NOT_FOUND
+
+        recipe.is_publish = True
+
+        return {}, HTTPStatus.NO_CONTENT
