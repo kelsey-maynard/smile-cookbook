89918aa9afb484f16dedd72d8e78f254de4231ee
Kelsey Maynard <kelsey@funnel.io>
2021-11-10 15:48:40 -0500

    Adding the delete method

--- resources/recipe.py
+++ resources/recipe.py
@@ -68,3 +68,13 @@ class RecipePublishResource(Resource):
         recipe.is_publish = True
 
         return {}, HTTPStatus.NO_CONTENT
+
+    def delete(self, recipe_id):
+        recipe = next((recipe for recipe in recipe_list if recipe.id == recipe_id), None)
+
+        if recipe is None:
+            return{'message': 'Recipe not found'}.HTTPStatus.NOT_FOUND
+
+        recipe.is_publish = False
+
+        return {}, HTTPStatus.NO_CONTENT
