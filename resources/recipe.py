4a31aa7bc2e41fede6406a9f6eddb4646db8787d
Kelsey Maynard <kelsey@funnel.io>
2021-11-10 07:41:01 -0500

    Created the RecipeListResource class

--- resources/recipe.py
+++ resources/recipe.py
@@ -4,3 +4,14 @@ from http import HTTPStatus
 
 from models.recipe import Recipe, recipe_list
 
+class RecipeListResource(Resource):
+
+    def get(self):
+
+        data = []
+
+        for recipe in recipe_list:
+            if recipe.is_publish is True:
+                data.append(recipe.data)
+
+            return{'data': data}, HTTPStatus.OK
