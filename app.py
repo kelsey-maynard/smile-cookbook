82e7b3514404db390c28ce4501a051e1c8aca8dc
Kelsey Maynard <kelsey@funnel.io>
2021-11-18 14:53:42 -0500

    updating app.py to create Flask app

--- app.py
+++ app.py
@@ -7,6 +7,27 @@ from extensions import db
 from models.user import User
 from resources.recipe import RecipeListResource, RecipeResource, RecipePublishResource
 
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    register_extensions(app)
+    register_resources(app)
+
+    return app
+
+def register_extensions(app):
+    db.init__app(app)
+    migrate = Migrate(app,db)
+
+def register_resources(app):
+    api=Api(app)
+
+    api.add_resource(RecipeListResource, '/recipes')
+    api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
+    api.add_resource(RecipePublishResource, '/recipes/<int:recipe_id>/publish')
+
+
 app = Flask(__name__)
 api = Api(app)
 
