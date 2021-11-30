e5926a833575f9e0dd7e7988ebd327f5897b883d
Kelsey Maynard <kelsey@funnel.io>
2021-11-30 12:53:57 -0500

    updating code so it works

--- app.py
+++ app.py
@@ -28,14 +28,6 @@ def register_resources(app):
     api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
     api.add_resource(RecipePublishResource, '/recipes/<int:recipe_id>/publish')
 
-
-app = Flask(__name__)
-api = Api(app)
-
-api.add_resource(RecipeListResource, '/recipes')
-api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
-api.add_resource(RecipePublishResource, '/recipes/<int:recipe_id>/publish')
-
 if __name__ == '__main__':
     app = create_app()
     app.run()
